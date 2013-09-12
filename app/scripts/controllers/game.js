a8cf18474643cb7cc6e21a9dcd32565455afebf8
Siroberto Scerbo <github@siroberto.com>
2013-09-12 14:51:07 -0400

    Added basic structure of board game model and view

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('ApocAcquireApp')
-  .controller('GameCtrl', function ($scope) {
+  .controller('GameCtrl', function ($scope, Board) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.tiles = Board.availTiles;
   });
--- app/scripts/services/board.js
+++ app/scripts/services/board.js
@@ -2,5 +2,35 @@
 
 angular.module('ApocAcquireApp')
   .service('Board', function Board() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+    // AngularJS will instantiate a singleton by calling 'new' on this function
+    this.availTiles = [];
+    this.availStocks = [];
+    this.availChains = [];
+    this.playedTiles = [];
+    this.existingChains = [];
+
+    this.getNextPlayer = function() {
+
+    };
+    this.getAvailTiles = function() {
+
+    };
+    this.getAvailStock = function(chain) {
+
+    };
+    this.getAvailChains = function() {
+
+    };
+    this.getPlayedTiles = function() {
+
+    };
+    this.getExistingChains = function() {
+
+    };
+    this.getTilesInChain = function(chain) {
+
+    };
+    this.getChainSize = function(chain) {
+
+    };
   });
