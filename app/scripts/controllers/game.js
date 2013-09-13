b71c34e2ec07f6957fb1e9057c283db15d8e7897
Siroberto Scerbo <github@siroberto.com>
2013-09-13 11:31:50 -0400

    Basic game board implemented in html with a cell selector function

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -8,5 +8,31 @@ angular.module('ApocAcquireApp')
       'Karma'
     ];
 
+    $scope.generateArrayOfNumbers = function(length) {
+    	var tempArray = [];
+
+    	for (var a=0;a<length;a++) {
+    		tempArray.push(a);
+    	}
+
+    	return tempArray;
+    }
+
+    $scope.boardWidth = 13;
+    $scope.boardHeight = 9;
+
+    $scope.rows = $scope.generateArrayOfNumbers($scope.boardHeight);
+    $scope.columns = $scope.generateArrayOfNumbers($scope.boardWidth);
+
+    $scope.companyNames = ['Festival','American','Imperial','Luxor','Worldwide','Tower','Continental'];
+    $scope.startingStockCount = 25;
+
+    Board.init($scope.boardWidth, $scope.boardHeight, $scope.companyNames, $scope.startingStockCount);
+
     $scope.tiles = Board.availTiles;
+    $scope.stocks = Board.availStocks;
+
+    $scope.selectBoardTile = function(row, column){
+      console.log(row, column);
+    };
   });
