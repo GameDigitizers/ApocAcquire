63ecb3e8eb43e2d5835589455e6c2c2dd7205953
Michael Stewart <michael_stewart+gitdev@vt.edu>
2013-09-14 13:53:37 -0400

    we're making our own awesomethings, don't need yeoman's defaults

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -2,12 +2,6 @@
 
 angular.module('ApocAcquireApp')
   .controller('GameCtrl', function ($scope, Board, Player, Action) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
     $scope.generateArrayOfNumbers = function(length) {
     	var tempArray = [];
 
