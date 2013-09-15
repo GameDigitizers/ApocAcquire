a33987eede29bd4684cdee0985678eed2da6563b
Siroberto Scerbo <github@siroberto.com>
2013-09-14 23:11:21 -0400

    Added tile model

---
+++ app/scripts/services/tile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function Tile($rootScope, column, row) {
+  this.$rootScope = $rootScope;
+
+  this.TILE_STATES = {'unplayed':0, 'played':1, 'unplayable':2} 
+
+  this.column = column;
+  this.row = row;
+  
+  this.state = this.TILE_STATES['unplayed']; //'unplayed', 'played', 
+  this.chain = undefined;
+
+  //this.sides; add later
+};
+
+Player.prototype.getNeighbors = function () {
+  //TODO: redifine this this.sides for future hexagon gameboards
+  
+  return true;
+};
+
+angular.module('ApocAcquireApp.services.tile', [])
+  .factory('Tile', function ($injector) {
+    return function(column, row) { 
+      return $injector.instantiate( Player, { 
+        column: column,
+        row: row
+      }); 
+    };
+  });
