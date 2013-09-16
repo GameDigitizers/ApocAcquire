3b5dc6aec2ede710acf2f88ece26ec39067d633f
Michael Stewart <michael_stewart+gitdev@vt.edu>
2013-09-15 20:35:46 -0400

    simplify actoin

--- app/scripts/services/action.js
+++ app/scripts/services/action.js
@@ -1,69 +1,31 @@
 'use strict';
 
 function Action($rootScope, requestedVerb, requestedArgs) {
-  this.VALID_VERBS = ['place-tile', 'buy-stock', 'sell-stock', 'trade-stock', 'draw-tile', 'hold-stock'];
   this.$rootScope = $rootScope;
-  this.verbValid = false;
-  this.argsValid = false;
-  this.verb;
-  this.args;
-
-  if (this.VALID_VERBS.indexOf(requestedVerb) >= 0) {
-    this.verb = requestedVerb;
-    this.verbValid = true;
-
-    if (this.verb == 'place-tile') {
-      if (requestedArgs.length == 1) {
-        if (requestedArgs[0].length == 2) {
-          this.args = requestedArgs;
-          this.argsValid = true;
-        }
-      }
-    } else if (this.verb == 'buy-stock') {
-      if (requestedArgs.length == 2) {
-        if (typeof(requestedArgs[0]) == 'number' && typeof(requestedArgs[1]) == 'string') {
-          this.args = requestedArgs;
-          this.argsValid = true;
-        }
-      }
-    } else if (this.verb == 'sell-stock') {
-      if (requestedArgs.length == 2) {
-        if (typeof(requestedArgs[0]) == 'number' && typeof(requestedArgs[1]) == 'string') {
-          this.args = requestedArgs;
-          this.argsValid = true;
-        }
-      }
-    } else if (this.verb == 'trade-stock') {
-      if (requestedArgs.length == 3) {
-        if (typeof(requestedArgs[0]) == 'number' && typeof(requestedArgs[1]) == 'string' && typeof(requestedArgs[2]) == 'string') {
-          this.args = requestedArgs;
-          this.argsValid = true;
-        }
-      }
-    } else if (this.verb == 'draw-tile') {
-      if (requestedArgs.length == 0) {
-        this.args = requestedArgs;
-        this.argsValid = true;
-      }
-    } else if (this.verb == 'hold-stock') {
-      if (requestedArgs.length == 2) {
-        if (typeof(requestedArgs[0]) == 'number' && typeof(requestedArgs[1]) == 'string') {
-          this.args = requestedArgs;
-          this.argsValid = true;
-        }
-      }
-    }
+  this.verb = requestedVerb;
+  this.args = requestedArgs;
+  this.act = function(){
+    Action.prototype.VERB_ACTIONS[this.verb](this.args);
   }
-};
+}
 
-Action.prototype.isValid = function () {
-  var valid = false;
+Action.prototype.VALID_VERBS = function(){
+  return Object.keys(Action.prototype.VERB_ACTIONS);
+}
 
-  if (this.verbValid && this.argsValid) {
-    valid = true;
+Action.prototype.VERB_ACTIONS = {
+  place_tile: function (args) { 
+  }, 
+  buy_stock: function (args) { 
+  }, 
+  sell_stock: function (args) { 
+  }, 
+  trade_stock: function (args) { 
+  }, 
+  draw_tile: function (args) { 
+  },
+  hold_stock: function (args) { 
   }
-
-  return valid;
 };
 
 angular.module('ApocAcquireApp.services.action', [])
